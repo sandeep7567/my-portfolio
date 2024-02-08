ad754c19cca67672d04ad202617148a2cf286da7
sandeep7567 <sandeepthakur7567@gmail.com>
2024-02-08 19:04:04 +0530

    add: social to z-index

--- components/segment/footer.tsx
+++ components/segment/footer.tsx
@@ -8,15 +8,15 @@ interface FooterProps {}
 
 export const Footer: FC<FooterProps> = ({}) => {
   return (
-      <div className="border-collapse border-t border-border/20 dark:border-white/20 bg-white dark:bg-transparent">
-    <Container>
+    <div className="border-collapse border-t border-border/20 dark:border-white/20 bg-white dark:bg-transparent">
+      <Container>
         <footer className="w-full flex flex-col md:flex-row gap-y-6 md:gap-y-0 p-8 md:px-6 justify-between items-center">
           <p className="text-xs md:text-sm dark:text-primary/70 text-primary-foreground/70">
             &copy; 2024 portfolio sandeep thakur All rights reserved.
           </p>
-          <Social />
+          <Social className="z-40" />
         </footer>
-    </Container>
-      </div>
+      </Container>
+    </div>
   );
 };
